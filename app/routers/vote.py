64afb3382d88f9b6b318e90102e51ddefa7bc3c7
Roshan Vyas <vyasroshan9000@gmail.com>
2026-05-31 22:58:58 +0530

    Added Dependency

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -7,11 +7,11 @@ from app.database import get_db
 from app.oauth2 import get_current_user
 
 router = APIRouter(
-    prefix='/vote',
+    prefix='/votes',
     tags=['Vote']
 )
 
-@router.post('/', status_code=status.HTTP_200_OK) # Since we doing both add and delete.
+@router.post('/', status_code=status.HTTP_200_OK) 
 def vote(
     vote: Vote,
     db: Session = Depends(get_db),
@@ -19,29 +19,29 @@ def vote(
     ):
     
     # Retrieving post.
-    post_exist = db.get(models.Post, vote.post_id)
+    existing_post = db.get(models.Post, vote.post_id)
 
     # Checking post exist or not.
-    if not post_exist:
+    if not existing_post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail='Post was not found'
         )
     
-    # Retrieving vote.
-    vote_exist = db.get(models.Vote, (vote.post_id, current_user.id))
+    # Retrieving user existing vote.
+    existing_vote = db.get(models.Vote, (vote.post_id, current_user.id))
     
     # If user wants to add vote(dir=1).
     if vote.dir == 1:
 
         # If vote already exist.
-        if vote_exist:
+        if existing_vote:
             raise HTTPException(
                 status_code=status.HTTP_409_CONFLICT,
                 detail='Your vote had already registered!'
             )
         
-        # Adding new_vote in votes table.
+        # Adding new_vote.
         new_vote = models.Vote(
             post_id=vote.post_id,
             user_id=current_user.id
@@ -54,14 +54,14 @@ def vote(
     
     else:
         # If vote not found.
-        if not vote_exist:
+        if not existing_vote:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail="You haven't voted yet!"
             )
         
         # Deleting vote.
-        db.delete(vote_exist)
+        db.delete(existing_vote)
         db.commit()
 
         return {'message': 'Successfully unvoted!'}
