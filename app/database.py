4fbda93e60aaee80d61e705f285ec6ef4af18979
Roshan Vyas <vyasroshan9000@gmail.com>
2026-05-31 23:04:29 +0530

    Minor updates

--- app/database.py
+++ app/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.config import settings
 
-# %40 is used for @ in password since we can't used directly in our url because it can only be used before host_name.
 SQLALCHEMY_DATABASE_URL = settings.database_url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
