4085e87ad949fa81f7772c00808196c519120053
Roshan Vyas <vyasroshan9000@gmail.com>
2026-05-31 22:44:57 +0530

    Added CorsMiddleWare

--- app/main.py
+++ app/main.py
@@ -7,19 +7,19 @@ from app.routers import post, user, auth, vote
 # Creating instance of an FastAPI
 app = FastAPI() 
 
-# Although we can specify any specific origins to work our applications(but here allowed for everyone).
+# Although we can specify any specific origins/websites to work our applications(but here allowed for everyone).
 origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],      # EVery methods(get, post, etc) can be used.
+    allow_methods=["*"],      # Allowed every methods(get, post, etc) can be used.
     allow_headers=["*"],
 )
 
 # It will create model/table in our database.
-# models.Base.metadata.create_all(bind=engine)   # Since we currently using alembic therefore we don't use this further.
+# models.Base.metadata.create_all(bind=engine)   # Since we currently using alembic(migration tool) therefore we don't use this further.
 
 app.include_router(post.router)
 app.include_router(user.router)
