b4069fdcf3f17851cc0b791fe898b470206a8620
Roshan Vyas <vyasroshan9000@gmail.com>
2026-05-31 23:05:45 +0530

    Added schema for token response

--- app/schemas.py
+++ app/schemas.py
@@ -2,19 +2,15 @@ from pydantic import BaseModel, Field, EmailStr
 from typing import Optional
 from datetime import datetime
 
-# Required attributes to create a post.
 class PostCreate(BaseModel):
     title: str = Field(min_length=1, max_length=120)
     content: str = Field(min_length=1)
     published: bool = True
-    # owner_id: int              # We don't need this to pass it from request explicitly (it would be fetched from current_user).
     
-# Required attribute to update a post.
 class PostUpdate(BaseModel):
     title: Optional[str] = Field(None, min_length=1, max_length=120)
     content: Optional[str] = Field(None, min_length=1)
     published: Optional[bool] = None
-    # owner_id: int | None = None  # Modern way of using optional.  # Illogical to add this (since a user cannot able to update anyone's owner_id)
     
 class PostResponse(BaseModel):
     id: int
@@ -23,7 +19,7 @@ class PostResponse(BaseModel):
     published: bool
     created_at: datetime
     owner_id: int
-    owner: 'UserResponse'   # Using '' because UserResponse haven't defined yet (now python won't throw error).
+    owner: 'UserResponse'   
 
     # Allowing conversion from orm objects using object attributes.
     model_config = {
@@ -32,7 +28,7 @@ class PostResponse(BaseModel):
 
 class PostVoteResponse(BaseModel):
     Post: PostResponse
-    votes: int = 0      # default for newly created post.
+    votes: int       
 
     # Allowing conversion from orm objects using object attributes.
     model_config = {
@@ -53,11 +49,7 @@ class UserResponse(BaseModel):
         'from_attributes': True
     }
 
-class LoginRequest(BaseModel):
-    email: EmailStr
-    password: str
-
-class LoginResponse(BaseModel):
+class TokenResponse(BaseModel):
     access_token: str
     token_type: str
 
