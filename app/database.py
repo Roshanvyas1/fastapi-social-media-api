aff01162c988d359d13da3f1c72cb768a2a3c776
Roshan Vyas <vyasroshan9000@gmail.com>
2026-06-05 16:35:36 +0530

    Replace sessionmaker to async_sessionmaker

--- app/database.py
+++ app/database.py
@@ -1,20 +1,21 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
 from app.config import settings
 
 SQLALCHEMY_DATABASE_URL = settings.database_url
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+AsyncSessionLocal = async_sessionmaker(
+                        bind=engine,
+                        expire_on_commit=False, 
+                        autoflush=False
+                    )
 
 class Base(DeclarativeBase):
     pass
 
 # Creating Session Dependency
-def get_db():
-    db = SessionLocal()
-    try: 
+async def get_db():
+    async with AsyncSessionLocal() as db:
         yield db
-    finally:
-        db.close()
