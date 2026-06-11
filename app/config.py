a04bcda8dace8faceac76cc32bbfad4d5b3cfd9a
Roshan Vyas <vyasroshan9000@gmail.com>
2026-06-11 20:25:16 +0530

    Updated class Settings and added ignore extra env feature

--- app/config.py
+++ app/config.py
@@ -1,10 +1,22 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    database_url: str
+    database_hostname: str
+    database_port: str
+    database_username: str
+    database_password: str
+    database_name: str
     secret_key: str
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 15
+
+    @property
+    def database_url(self):
+        return (
+            f"postgresql+psycopg://{self.database_username}:{self.database_password}"
+            f"@{self.database_hostname}:{self.database_port}/{self.database_name}"
+        )
+
 
     model_config = SettingsConfigDict(
         env_file=".env",
