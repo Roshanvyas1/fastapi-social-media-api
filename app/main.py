ec24f5cdcd7eb0c9b1ee21afdfd6f295a2cbeba8
Roshan Vyas <vyasroshan9000@gmail.com>
2026-06-16 03:03:04 +0530

    Add health check and rate limiter

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.ext.asyncio import AsyncSession
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from app.limiter import limiter
+from sqlalchemy import text
 from app.routers import post, user, auth, vote
+from app.database import get_db
 
 # Creating instance of an FastAPI
 app = FastAPI()
+
+# Makes the limiter available to middleware.
+app.state.limiter = limiter
+
+# Converts slowapi exception into HTTP response.
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)  # type: ignore
 
 # Although we can specify any specific origins/websites to work our applications(but here allowed for everyone).
 origins = ["*"]
@@ -11,7 +23,7 @@ origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,  # Not needed for jwt authentication.
     allow_methods=["*"],  # Allowed every methods(get, post, etc) can be used.
     allow_headers=["*"],
 )
@@ -25,6 +37,13 @@ app.include_router(auth.router)
 app.include_router(vote.router)
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello, Dunia"}
+@app.get("/health", tags=["Health-Check"])
+async def health(db: AsyncSession = Depends(get_db)):
+    try:
+        await db.execute(text("SELECT 1"))
+        return {"status": "ok", "db": "connected"}
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Database Unavailable",
+        )
