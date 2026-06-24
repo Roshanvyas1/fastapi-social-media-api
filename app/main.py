f7f82c7bca7d1617de29a9b224652cf62bb106cf
Roshan Vyas <vyasroshan9000@gmail.com>
2026-06-25 02:57:42 +0530

    Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -41,7 +41,10 @@ app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)  # type: ignore
 
 # Although we can specify any specific origins/websites to work our applications.
-origins = ["https://fastapi-social-media-api.vercel.app/"]
+origins = [
+    "https://fastapi-social-media-api.vercel.app",
+    "http://localhost:8000"
+]
 
 app.add_middleware(
     CORSMiddleware,
